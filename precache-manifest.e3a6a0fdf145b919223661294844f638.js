e096e0c44ddea81df232749b1a55e82b8ed9f401
Akshaychavan41 <aschavan41@gmail.com>
2021-07-04 13:50:46 +0530

    Updates

---
+++ precache-manifest.e3a6a0fdf145b919223661294844f638.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0acf90ea549641268dc9abb006eb2db5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7eb49f2d28593a854fcb",
+    "url": "/static/css/main.09d5db06.chunk.css"
+  },
+  {
+    "revision": "1996ad115d4ec1054c1c",
+    "url": "/static/js/2.b135b49d.chunk.js"
+  },
+  {
+    "revision": "7eb49f2d28593a854fcb",
+    "url": "/static/js/main.e40cc72a.chunk.js"
+  },
+  {
+    "revision": "d639821073d1caea3446",
+    "url": "/static/js/runtime-main.666bd3de.js"
+  }
+]);
